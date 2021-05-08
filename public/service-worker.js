1482500c6194ab7225da5dbd018f693b9f31862f
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-08 14:44:51 -0400

    creating service worker document in public folder

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/icon-192x192.png",
+    "/icon-512x512.png",
+    "db.js",
+    "index.js",
+    "manifest.webmanifest",
+    "style.css",
+    "favicon.ico",
+    "service-worker.js"
+];
+
