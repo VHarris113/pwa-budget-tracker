7fcde60172c90b436080c7d3ffb1c741d56a3da7
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-08 18:37:30 -0400

    editing the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = server.listen(process.env.PORT || 3001);
 
 const app = express();
 
