3b4d8646bfe3b7acceecca36464ac446d93e6678
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-08 17:41:57 -0400

    still modifying service-workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "/",
     "/db.js",
@@ -7,9 +10,6 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "icons/icon-512x512.png"
 ];
-
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", evt => {
     evt.waitUntil(
@@ -24,29 +24,32 @@ self.addEventListener("install", evt => {
 self.addEventListener("fetch", evt => {
     if (evt.request.url.includes("/api/")) {
         console.log('[Server Worker] Fetch(data)', evt.request.url);
-
-    evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(async cache => {
-          try {
-                const response = await fetch(evt.request);
-                if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                }
-                return response;
-            } catch (err) {
-                return await cache.match(evt.request);
-            }
-        })
-    )
-    return;
+        evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
     }
-  
     evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
+        fetch(evt.request).catch(() => {
+            return caches.match(evt.request).then((response) => {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
     );
-  });
+});
   
