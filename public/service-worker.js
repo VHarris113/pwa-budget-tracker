d64f180b3a790160d42e1305bf92048fd0f2072f
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-08 16:29:44 -0400

    working on db file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
 const FILES_TO_CACHE = [
     "/",
-    "/public/index.html",
-    "/public/icons/icon-192x192.png",
-    "/public/icons/icon-512x512.png",
-    "/public/db.js",
-    "/public/index.js",
-    "/public/manifest.webmanifest",
-    "/public/style.css",
-    "/public/favicon.ico",
-    "/public/service-worker.js"
+    "index.html",
+    "/icons/icon-192x192.png",
+    "icons/icon-512x512.png",
+    "db.js",
+    "index.js",
+    "manifest.webmanifest",
+    "style.css",
+    "favicon.ico",
+    "service-worker.js"
 ];
 
 const CACHE_NAME = "static-cache-v1";
@@ -16,25 +16,21 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE)
+        caches.open(CACHE_NAME).then((c) => {
+            return c.addAll(FILES_TO_CACHE)
         })
     )
     .catch((err) => {
         console.log("Add all error", err);
     })
-        .catch((err) => {
-            console.log(err);
-        });
-
     self.skipWaiting();
 });
 
 self.addEventListener("activate", function(evt) {
     evt.waitUntil(
-      caches.keys().then(keyList => {
+      caches.keys().then((keyList) => {
         return Promise.all(
-          keyList.map(key => {
+          keyList.map((key) => {
             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
               console.log("Removing old cache data", key);
               return caches.delete(key);
@@ -50,19 +46,19 @@ self.addEventListener("activate", function(evt) {
 self.addEventListener("fetch", function(evt) {
     if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
         evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
+            caches.open(DATA_CACHE_NAME).then((cache) => {
             return fetch(evt.request)
-            .then(response => {
+            .then((response) => {
                 if (response.status === 200) {
                   cache.put(evt.request.url, response.clone());
                 }
                 return response;
               })
-              .catch(() => {
+              .catch((cache) => {
                 return cache.match(evt.request);
               });
           })
-          .catch(err => console.log(err))
+          .catch((err) => console.log(err))
         );
         return;
         }
