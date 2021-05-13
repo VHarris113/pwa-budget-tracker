7ff8dd25715ac65b99d5f6aabdd2720459d6d391
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-13 09:40:47 -0400

    fixing issues with this

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,55 +1,60 @@
-const CACHE_NAME = "static-cache-v1";
+var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const FILES_TO_CACHE = [
-    "/",
-    "/db.js",
-    "/index.js",
-    "/manifest.json",
-    "/style.css",
-    "/icons/icon-192x192.png",
-    "icons/icon-512x512.png"
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
-self.addEventListener("install", evt => {
-    evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-  
 
-self.addEventListener("fetch", evt => {
-    if (evt.request.url.includes("/api/")) {
-        console.log('[Server Worker] Fetch(data)', evt.request.url);
-        evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-            return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            }).catch(err => console.log(err))
-        );
-        return;
-    }
-    evt.respondWith(
-        fetch(evt.request).catch(() => {
-            return caches.match(evt.request).then((response) => {
-                if (response) {
-                    return response;
-                } else if (evt.request.headers.get("accept").includes("text/html")) {
-                    return caches.match("/");
-                }
-            });
-        })
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
-  
