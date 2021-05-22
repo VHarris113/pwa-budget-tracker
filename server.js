2ac71374e3b7a33257e6c6f4251ec26097113e03
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-22 13:50:58 -0400

    working on database connection issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.MONGODB_URI || 3000;
 
 const app = express();
 
